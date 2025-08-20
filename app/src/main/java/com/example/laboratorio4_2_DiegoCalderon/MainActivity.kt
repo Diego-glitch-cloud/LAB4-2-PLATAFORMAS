b6e60239bda95f03b829b23cbfbd205f44a4d889
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-08-19 20:31:47 -0600

    primer boton

--- app/src/main/java/com/example/laboratorio4_2_DiegoCalderon/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio4_2_DiegoCalderon/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -15,8 +18,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.laboratorio4_2_DiegoCalderon.ui.theme.Laboratorio4_2Theme
-import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,31 +27,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Laboratorio4_2Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    // Variables para almacenar los datos de la receta
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     var receta by remember { mutableStateOf("") }
                     var url by remember { mutableStateOf("") }
                     val listaRecetas = remember { mutableStateListOf<String>() }
 
-                    Column (
+                    Row(
                         modifier = Modifier
                             .padding(innerPadding)
+                            .padding(16.dp)
                             .fillMaxSize()
                     ) {
-                        TextField(
-                            value = receta,
-                            onValueChange = { receta = it }, // Actualiza el valor de receta dependiendo de lo que se ingrese
-                            label = { Text("Nombre de la Receta") }
-                        )
-                        TextField(
-                            value = url,
-                            onValueChange = { url = it },
-                            label = { Text("URL de la imagen")}
-                        )
+                        Column(
+                            modifier = Modifier
+                                .weight(1f) // Esto hace que ocupe todo el espacio disponible
+                        ) {
+                            TextField(
+                                value = receta,
+                                onValueChange = { receta = it },
+                                label = { Text("Nombre de la Receta") }
+                            )
+                            TextField(
+                                value = url,
+                                onValueChange = { url = it },
+                                label = { Text("URL de la imagen") }
+                            )
+                        }
+
+                        Button(
+                            onClick = {
+                                // Lógica para agregar a la lista
+                            }
+                        ) {
+                            Text("Agregar receta")
+                        }
                     }
                 }
             }
         }
     }
 }
-
