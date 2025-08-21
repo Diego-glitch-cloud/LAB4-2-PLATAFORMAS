168f73c8ea1f7967237a56622396820aa0b4e590
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-08-20 18:46:09 -0600

    codigo completo

--- app/src/main/java/com/example/laboratorio4_2_DiegoCalderon/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio4_2_DiegoCalderon/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -23,16 +29,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.laboratorio4_2_DiegoCalderon.ui.theme.Laboratorio4_2Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Laboratorio4_2Theme {
+            Laboratorio4_2Theme(dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     var receta by remember { mutableStateOf("") }
                     var url by remember { mutableStateOf("") }
@@ -42,14 +52,17 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .padding(innerPadding)
                             .padding(16.dp)
-                            .fillMaxSize(),
                     ) {
                         Row(
-                            modifier = Modifier.fillMaxWidth(),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(IntrinsicSize.Min),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Column(
-                                modifier = Modifier.weight(1f),
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight(),
                             ){
                                 TextField(
                                     value = receta,
@@ -64,30 +77,54 @@ class MainActivity : ComponentActivity() {
                             }
                             Button(
                                 onClick = {
-
-                                    if (receta.isNotBlank() && !listaRecetas.contains(receta) && url.isNotBlank()) { // Verificación
-                                        listaRecetas.add(receta)
-
-                                        // Se limpian los campos
+                                    val nuevaReceta = Receta(receta, url)
+                                    if (receta.isNotBlank() && url.isNotBlank() && !listaRecetas.contains(nuevaReceta)) {
+                                        listaRecetas.add(nuevaReceta)
                                         receta = ""
                                         url = ""
                                     }
                                 },
-                                colors = ButtonDefaults.buttonColors(containerColor = Color.Green) // Boton Verde
+                                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+                                modifier = Modifier.fillMaxHeight()
                             ) {
-                                Text("Agregar receta")
+                                Text("Agregar receta", fontSize = 18.sp)
                             }
                         }
 
-                        // Mostrar lazy column
                         LazyColumn(
                             modifier = Modifier.weight(1f)
+                                .fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            items(listaRecetas) { receta -> Text(text = receta) }
+                            items(listaRecetas) { receta ->
+                                RecetaCard(receta = receta)
+                            }
                         }
                     }
                 }
             }
+        }
+    }
+
+    @Composable
+    fun RecetaCard(receta: Receta) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = receta.imageUrl,
+                contentDescription = "Imagen de la receta ${receta.nombre}",
+                modifier = Modifier.size(300.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            )
+            Text(
+                text = receta.nombre,
+                fontSize = 30.sp,
+                modifier = Modifier.padding(top=8.dp)
+            )
         }
     }
 }
